16d3040474c864b058487307faea4a03f6196b47
BobKaLonger <rkollingeriii@gmail.com>
2026-07-13 22:32:54 -0400

    added config/paintdata for DeluxePlus, updated file structure for DeluxePlus, reverted customlights, started barn and coop item moves

--- Code/ModEntry.cs
+++ Code/ModEntry.cs
@@ -21,6 +21,8 @@ namespace PremiumCoopAndBarn
         internal const string PremiumCP = "bobkalonger.PremiumCoopAndBarnCP_";
         internal const string PremiumBarn = $"{PremiumCP}PremiumBarn";
         internal const string PremiumCoop = $"{PremiumCP}PremiumCoop";
+        internal const string DeluxePlusBarn = $"{PremiumCP}DeluxePlusBarn";
+        internal const string DeluxePlusCoop = $"{PremiumCP}DeluxePlusCoop";
         public override void Entry(IModHelper helper)
         {
             if (helper.ModRegistry.IsLoaded("FlashShifter.StardewValleyExpandedCP"))
@@ -40,19 +42,13 @@ namespace PremiumCoopAndBarn
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
-        private static bool HasSecondDoor(Building b) =>
-            b.buildingType.Value == PremiumBarn && b.tilesWide.Value == 11;
-
-        private static bool IsSVECoop(Building b) =>
-            b.buildingType.Value == PremiumCoop && b.tilesWide.Value == 9;
-
         private void PlayerOnWarped(object? sender, WarpedEventArgs e)
         {
             RemoveCustomlights(e.OldLocation);
 
             foreach (var b in e.NewLocation.buildings)
             {
-                if (HasSecondDoor(b))
+                if (b.buildingType.Value == PremiumBarn)
                 {
                     var premiumLightBL = new Point(b.tileX.Value + 3, b.tileY.Value + 3);
                     var ll = new LightSource($"{PremiumCP}BarnLight_{b.tileX.Value}_{b.tileY.Value}_L", 4, premiumLightBL.ToVector2() * Game1.tileSize, 1f, Color.Black, LightSource.LightContext.None);
@@ -63,7 +59,7 @@ namespace PremiumCoopAndBarn
                     Game1.currentLightSources.Add(lr.Id, lr);
                 }
 
-                if (IsSVECoop(b))
+                if (b.buildingType.Value == PremiumCoop)
                 {
                     var premiumLightC = new Point(b.tileX.Value + 6, b.tileY.Value + 2);
                     var lc = new LightSource($"{PremiumCP}CoopLight_{b.tileX.Value}_{b.tileY.Value}", 4, premiumLightC.ToVector2() * Game1.tileSize, 1f, Color.Black, LightSource.LightContext.None);
@@ -88,7 +84,7 @@ namespace PremiumCoopAndBarn
         {
             Utility.ForEachBuilding(building =>
             {
-                if (building.buildingType.Value is PremiumBarn or PremiumCoop)
+                if (building.buildingType.Value is PremiumBarn or PremiumCoop or DeluxePlusBarn or DeluxePlusCoop)
                 {
                     var interior = building.GetIndoors();
 
@@ -106,7 +102,7 @@ namespace PremiumCoopAndBarn
         {
             Utility.ForEachBuilding(building =>
             {
-                if (building.buildingType.Value is PremiumBarn or PremiumCoop)
+                if (building.buildingType.Value is PremiumBarn or PremiumCoop or DeluxePlusBarn or DeluxePlusCoop)
                 {
                     var interior = building.GetIndoors();
 
@@ -118,6 +114,106 @@ namespace PremiumCoopAndBarn
                 }
                 return true;
             });
+
+            Utility.ForEachBuilding(building =>
+            {
+                if (building.buildingType.Value is not (PremiumBarn or PremiumCoop or DeluxePlusBarn or DeluxePlusCoop))
+                    return true;
+
+                var interior = building.GetIndoors();
+
+                if (building.daysUntilUpgrade.Value > 0 || interior == null)
+                    return true;
+
+                string upgradeKey = $"{ModManifest.UniqueID}/buildingKey";
+                string currentLevel = building.buildingType.Value;
+                building.modData.TryGetValue(upgradeKey, out string lastMovedLevel);
+
+                if (lastMovedLevel != currentLevel)
+                {
+                    if (building.buildingType.Value is PremiumBarn or DeluxePlusBarn)
+                        BarnItemMoves(interior);
+                    else if (building.buildingType.Value is PremiumCoop or DeluxePlusCoop)
+                        CoopItemMoves(interior);
+
+                    building.modData[upgradeKey] = currentLevel;
+                }
+                return true;
+            });
+        }
+
+        private static List<(Vector2 tile, StardewValley.Object obj)> SpiralSearch(GameLocation location, string qualifiedID, Vector2 center, int maxRadius)
+        {
+            var results = new List<(Vector2, StardewValley.Object)>();
+
+            for (int radius = 0; radius <= maxRadius; radius++)
+            {
+                for (int dx = -radius; dx <= radius; dx++)
+                {
+                    for (int dy = -radius; dy <= radius; dy++)
+                    {
+                        if (Math.Abs(dx) != radius && Math.Abs(dy) != radius)
+                            continue;
+
+                        Vector2 tile = new Vector2(center.X + dx, center.Y + dy);
+                        if (location.objects.TryGetValue(tile, out StardewValley.Object obj) && obj.QualifiedItemId == qualifiedID)
+                        {
+                            results.Add((tile, obj));
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        private static Vector2 LandingPadRect(GameLocation location, Rectangle landingPad)
+        {
+            for (int y = landingPad.Top; y < landingPad.Bottom; y++)
+            {
+                for (int x = landingPad.Left; x < landingPad.Right; x++)
+                {
+                    Vector2 candidate = new Vector2(x, y);
+                    if (!location.IsTileBlockedBy(candidate, CollisionMask.Objects | CollisionMask.Furniture))
+                    {
+                        return candidate;
+                    }
+                }
+            }
+            return Vector2.Zero;
+        }
+
+        private static void BarnItemMoves(GameLocation interior)
+        {
+            if (interior.map == null) return;
+
+            var namedDestinations = new Dictionary<string, Vector2>
+            {
+                { "(BC)99",  new Vector2( 4,  3) },
+                { "(BC)104", new Vector2(23,  3) },
+                { "(BC)165", new Vector2(10, 16) },
+                { "(BC)272", new Vector2(17, 16) }
+            };
+
+            //var spawnIfMissing = new HashSet<string> { "(BC)104", "(BC)165", "(BC)272" };
+            //   **is it possible to gate this for only Premium buildings and exclude the spawn in DeluxePlus buildings???
+
+            //var haySlots = new List<Rectangle>
+            //   **this doesn't need to be a list anymore, it's just a single rectangle...
+        }
+
+        private static void CoopItemMoves(GameLocation interior)
+        {
+            if (interior.map == null) return;
+
+            var namedDestinations = new Dictionary<string, Vector2>
+            {
+                { "(BC)99",  new Vector2(22,  3) },
+                { "(BC)101", new Vector2( 3,  3) },
+                { "(BC)104", new Vector2(23,  3) },
+                { "(BC)165", new Vector2(10, 12) },
+                { "(BC)272", new Vector2(15, 12) }
+            };
         }
 
         [HarmonyPatch(typeof(Building), nameof(Building.doesTileHaveProperty))]
@@ -125,7 +221,7 @@ namespace PremiumCoopAndBarn
         {
             public static void Postfix(Building __instance, int tile_x, int tile_y, string property_name, string layer_name, ref string property_value, ref bool __result)
             {
-                if (HasSecondDoor(__instance) && __instance.daysOfConstructionLeft.Value <= 0)
+                if (__instance.buildingType.Value == PremiumBarn && __instance.daysOfConstructionLeft.Value <= 0)
                 {
                     var interior = __instance.GetIndoors();
                     if (tile_x == __instance.tileX.Value + __instance.humanDoor.X + 8 &&
@@ -150,7 +246,7 @@ namespace PremiumCoopAndBarn
                 if (who.ActiveObject != null && who.ActiveObject.IsFloorPathItem() && who.currentLocation != null && !who.currentLocation.terrainFeatures.ContainsKey(tileLocation))
                     return;
 
-                if (HasSecondDoor(__instance) && __instance.daysOfConstructionLeft.Value <= 0)
+                if (__instance.buildingType.Value == PremiumBarn && __instance.daysOfConstructionLeft.Value <= 0)
                 {
                     var interior = __instance.GetIndoors();
                     if (tileLocation.X == __instance.tileX.Value + __instance.humanDoor.X + 8 &&
@@ -188,7 +284,7 @@ namespace PremiumCoopAndBarn
         {
             public static void Postfix(Building __instance, GameLocation interior)
             {
-                if (!HasSecondDoor(__instance))
+                if (__instance.buildingType.Value != PremiumBarn)
                     return;
                 if (interior == null || interior.warps.Count < 2)
                     return;
@@ -203,18 +299,19 @@ namespace PremiumCoopAndBarn
         {
             public static void Postfix(GameLocation location, string buildingId, ref bool __result)
             {
-                string? toCheck = null;
-                if (buildingId == "Barn" || buildingId == "Big Barn" || buildingId == "Deluxe Barn")
-                    toCheck = PremiumBarn;
-                else if (buildingId == "Coop" || buildingId == "Big Coop" || buildingId == "Deluxe Coop")
-                    toCheck = PremiumCoop;
+                if (__result) return;
 
-                if (!__result && toCheck != null)
+                if (buildingId == "Barn" || buildingId == "Big Barn" || buildingId == "Deluxe Barn")
                 {
-                    if (location.getNumberBuildingsConstructed(toCheck) > 0)
-                    {
+                    if (location.getNumberBuildingsConstructed(PremiumBarn) > 0 ||
+                        location.getNumberBuildingsConstructed(DeluxePlusBarn) > 0)
                         __result = true;
-                    }
+                }
+                else if (buildingId == "Coop" || buildingId == "Big Coop" || buildingId == "Deluxe Coop")
+                {
+                    if (location.getNumberBuildingsConstructed(PremiumCoop) > 0 ||
+                        location.getNumberBuildingsConstructed(DeluxePlusCoop) > 0)
+                        __result = true;
                 }
             }
         }
@@ -227,7 +324,9 @@ namespace PremiumCoopAndBarn
                 if (!forConstruction)
                     return;
                 if (__instance.buildingType.Value != PremiumBarn &&
-                    __instance.buildingType.Value != PremiumCoop)
+                    __instance.buildingType.Value != PremiumCoop &&
+                    __instance.buildingType.Value != DeluxePlusBarn &&
+                    __instance.buildingType.Value != DeluxePlusCoop)
                     return;
 
                 foreach (var obj in __instance.indoors.Value.Objects.Values)
